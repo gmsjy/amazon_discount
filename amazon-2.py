423bc25a23215e0ad594df4bbfbdff30b27b830e
gmsjy <ZTXYYCX@hotmail.com>
2016-05-31 13:20:02 +0800

    add the file for 2.7

---
+++ amazon-2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from urllib2 import urlopen
+from bs4 import BeautifulSoup
+
+amazon = "https://www.amazon.cn/gp/feature.html/ref=sa_menu_kindle_l3_f126758?ie=UTF8&docId=126758"
+
+def getInfo(URL):
+    html = urlopen(URL)
+    data = html.read()
+    soup = BeautifulSoup(data)
+    dicounts = soup.findAll(attrs={"class": "gridProductContainer"})
+
